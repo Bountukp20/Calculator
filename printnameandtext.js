64d7f8209d4171b5f7750ee3778f061d661a1f8f
Beauty <105244471+Bountukp20@users.noreply.github.com>
2022-06-20 12:03:22 -0500

    Create printnameandtext.js

---
+++ printnameandtext.js
@@ -0,0 +1,5 @@
+function readMe(){
+  var show = document.getElementById("show");
+  show.innerText = "Hello, I am Marvel Ukpong, I am 175cm tall and I'm a Nigerian."
+  show.style.color = "darkgreen"
+}
